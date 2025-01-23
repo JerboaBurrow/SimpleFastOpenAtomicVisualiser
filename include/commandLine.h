2626f996dd235d81f42f744d7f1b9ae7d15c271a
Jerboa-app <dev@jerboa.app>
2025-01-23 08:22:15 +0000

    Structure path is position argument 1

--- include/commandLine.h
+++ include/commandLine.h
@@ -37,6 +37,7 @@ struct Argument
     T defaultValue;
     T value;
     bool required;
+    uint8_t position;
 
     /**
      * @brief Construct a new Argument
@@ -46,19 +47,22 @@ struct Argument
      * @param description the help description.
      * @param defaultValue the default value.
      * @param required if the Argument must be passed.
+     * @param position if the Argument must be in a set position.
      */
     Argument
     (
         std::string name,
         std::string description,
         T defaultValue,
-        bool required = false
+        bool required = false,
+        uint8_t position = 0
     )
     : name(name),
       description(description),
       defaultValue(defaultValue),
       value(defaultValue),
-      required(required)
+      required(required),
+      position(position)
     {}
 
     /**
@@ -69,10 +73,15 @@ struct Argument
     std::string help() const
     {
         std::stringstream h;
-        h << " -"
-          << name
-          << "\n "
-          << "  " << description;
+        if (position > 0)
+        {
+            h << "Positional argument " << int(position);
+        }
+        else
+        {
+            h << " -" << name;
+        }
+        h  << "\n  " << description;
         if (required)
         {
             h << "\n   Default: none."
@@ -101,9 +110,11 @@ struct Argument
  * @param c the entry to check.
  * @param count the size of commandLine.
  * @remark If arg.name is not at commandLine[c] nothing happens.
+ * @return true the argument was read."
+ * @return false the argument was not read."
  */
 template <class T>
-void getArgument
+bool getArgument
 (
     Argument<T> & arg,
     char ** commandLine,
@@ -120,9 +131,11 @@ void getArgument
  * @param c the entry to check.
  * @param count the size of commandLine.
  * @remark If arg.name is not at commandLine[c] nothing happens.
+ * @return true the argument was read."
+ * @return false the argument was not read."
  */
 template <>
-void getArgument<uint8_t>
+bool getArgument<uint8_t>
 (
     Argument<uint8_t> & arg,
     char ** commandLine,
@@ -130,10 +143,17 @@ void getArgument<uint8_t>
     const uint8_t count
 )
 {
+    if (c == arg.position)
+    {
+        arg.value = std::stoi(commandLine[c]);
+        return true;
+    }
     if (c < count-1  && startsWith(commandLine[c], arg.name))
     {
         arg.value = std::stoi(commandLine[c+1]);
+        return true;
     }
+    return false;
 }
 
 /**
@@ -145,9 +165,11 @@ void getArgument<uint8_t>
  * @param c the entry to check.
  * @param count the size of commandLine.
  * @remark If arg.name is not at commandLine[c] nothing happens.
+ * @return true the argument was read."
+ * @return false the argument was not read."
  */
 template <>
-void getArgument<float>
+bool getArgument<float>
 (
     Argument<float> & arg,
     char ** commandLine,
@@ -155,10 +177,17 @@ void getArgument<float>
     const uint8_t count
 )
 {
+    if (c == arg.position)
+    {
+        arg.value = std::stof(commandLine[c]);
+        return true;
+    }
     if (c < count-1  && startsWith(commandLine[c], arg.name))
     {
         arg.value = std::stof(commandLine[c+1]);
+        return true;
     }
+    return false;
 }
 
 /**
@@ -170,9 +199,12 @@ void getArgument<float>
  * @param c the entry to check.
  * @param count the size of commandLine.
  * @remark If arg.name is not at commandLine[c] nothing happens.
+ * @remark If commandLine[c] does not exist as a path a std::runtime_error is thrown.
+ * @return true the argument was read."
+ * @return false the argument was not read."
  */
 template <>
-void getArgument<std::filesystem::path>
+bool getArgument<std::filesystem::path>
 (
     Argument<std::filesystem::path> & arg,
     char ** commandLine,
@@ -180,10 +212,25 @@ void getArgument<std::filesystem::path>
     const uint8_t count
 )
 {
+    if (c == arg.position)
+    {
+        arg.value = std::filesystem::path(commandLine[c]);
+        if (!std::filesystem::exists(arg.value))
+        {
+            throw std::runtime_error(std::string("Path: ") + commandLine[c] + " does not exist.");
+        }
+        return true;
+    }
     if (c < count-1  && startsWith(commandLine[c], arg.name))
     {
         arg.value = std::filesystem::path(commandLine[c+1]);
+        if (!std::filesystem::exists(arg.value))
+        {
+            throw std::runtime_error(std::string("Path: ") + commandLine[c] + " does not exist.");
+        }
+        return true;
     }
+    return false;
 }
 
 /**
@@ -195,9 +242,11 @@ void getArgument<std::filesystem::path>
  * @param c the entry to check.
  * @param count the size of commandLine.
  * @remark If arg.name is not at commandLine[c] nothing happens.
+ * @return true the argument was read."
+ * @return false the argument was not read."
  */
 template <>
-void getArgument<BASE_MESH>
+bool getArgument<BASE_MESH>
 (
     Argument<BASE_MESH> & arg,
     char ** commandLine,
@@ -205,10 +254,17 @@ void getArgument<BASE_MESH>
     const uint8_t count
 )
 {
-    if (c < count-1  && startsWith(commandLine[c], arg.name))
+    if (c == arg.position)
+    {
+        arg.value = BASE_MESH(std::min(uint8_t(std::stoi(commandLine[c])), uint8_t(uint8_t(BASE_MESH::ANY))));
+        return true;
+    }
+    if (c < count-1 && startsWith(commandLine[c], arg.name))
     {
         arg.value = BASE_MESH(std::min(uint8_t(std::stoi(commandLine[c+1])), uint8_t(uint8_t(BASE_MESH::ANY))));
+        return true;
     }
+    return false;
 }
 
 /**
@@ -220,10 +276,12 @@ void getArgument<BASE_MESH>
  * @param c the entry to check.
  * @param count the size of commandLine.
  * @remark If arg.name is not at commandLine[c] nothing happens.
+ * @return true the argument was read."
+ * @return false the argument was not read."
  * @remark Acts as a switch, i.e. no argument is required.
  */
 template <>
-void getArgument<bool>
+bool getArgument<bool>
 (
     Argument<bool> & arg,
     char ** commandLine,
@@ -231,10 +289,12 @@ void getArgument<bool>
     const uint8_t count
 )
 {
-    if (startsWith(commandLine[c], arg.name))
+    if (c == arg.position || startsWith(commandLine[c], arg.name))
     {
         arg.value = true;
+        return true;
     }
+    return false;
 }
 
 /**
@@ -253,6 +313,12 @@ struct CommandLine
     CommandLine(uint8_t count, char ** commandLine)
     {
         if (count == 1) { help(); return; }
+        if (!getArgument<std::filesystem::path>(structure, commandLine, structure.position, count))
+        {
+            std::cout << "A structure path must be given as argument 1\n";
+            std::exit(EXIT_FAILURE);
+        }
+
         for (uint8_t c = 1; c < count; c++)
         {
             checkHelp(commandLine[c]);
@@ -260,7 +326,6 @@ struct CommandLine
             getArgument<uint8_t>(msaa, commandLine, c, count);
             getArgument<bool>(meshes, commandLine, c, count);
             getArgument<BASE_MESH>(mesh, commandLine, c, count);
-            getArgument<std::filesystem::path>(structure, commandLine, c, count);
             getArgument<float>(bondCutoff, commandLine, c, count);
             getArgument<float>(bondSize, commandLine, c, count);
             getArgument<bool>(hideAtoms, commandLine, c, count);
@@ -271,7 +336,7 @@ struct CommandLine
     Argument<uint8_t> msaa = {"msaa", "MSAA level [0-32].", 0, false};
     Argument<BASE_MESH> mesh = {"mesh", "The procedural mesh type.", BASE_MESH::ANY, false};
     Argument<bool> meshes = {"meshes", "Whether to use meshes for atoms.", false, false};
-    Argument<std::filesystem::path> structure = {"atoms", "The structure path.", {}, true};
+    Argument<std::filesystem::path> structure = {"atoms", "The structure path.", {}, true, 1};
     Argument<float> bondCutoff = {"bondCutOff","Angstrom cutoff to create a bond.", 0.0f, false};
     Argument<float> bondSize = {"bondSize", "The size of bonds.", 1.0f, false};
     Argument<bool> hideAtoms = {"hideAtoms", "Whether to hide atoms (toggle-able at runtime).", false, false};
