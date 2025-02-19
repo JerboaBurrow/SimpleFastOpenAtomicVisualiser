008438a931bc174e2363f9889e35349c0b749e24
Jerboa-app <84378622+Jerboa-app@users.noreply.github.com>
2025-02-19 07:13:13 +0000

    Deploying to docs from @ JerboaBurrow/SimpleFastOpenAtomicVisualiser@1d84bc930df11f2a58b298e496598b37138b177b 🚀

--- menudata.js
+++ menudata.js
@@ -120,6 +120,7 @@ var menudata={children:[
 {text:"v",url:"globals.html#index_v"}]},
 {text:"Functions",url:"globals_func.html",children:[
 {text:"a",url:"globals_func.html#index_a"},
+{text:"b",url:"globals_func.html#index_b"},
 {text:"c",url:"globals_func.html#index_c"},
 {text:"d",url:"globals_func.html#index_d"},
 {text:"e",url:"globals_func.html#index_e"},
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['beginning_0',['beginning',['../classStructure.html#a1f3a3ec209e4c80e32c067071966d289',1,'Structure']]],
-  ['bond_1',['Bond',['../structBond.html#a0e1531db24b2cb1832619ec3e0c9d912',1,'Bond']]],
-  ['bondrenderer_2',['BondRenderer',['../classBondRenderer.html#a857d2c2ae7a9e8ee8db5ab64011f3cf5',1,'BondRenderer']]],
-  ['build_3',['build',['../classHierarchicalTriangularMesh.html#a1a3cef433ecdd66eeeaa603eeb7aa617',1,'HierarchicalTriangularMesh']]]
+  ['backward_0',['backward',['../main_8h.html#ab53b340979ca77d0273c30c705393c60',1,'main.h']]],
+  ['beginning_1',['beginning',['../classStructure.html#a1f3a3ec209e4c80e32c067071966d289',1,'Structure']]],
+  ['bond_2',['Bond',['../structBond.html#a0e1531db24b2cb1832619ec3e0c9d912',1,'Bond']]],
+  ['bondrenderer_3',['BondRenderer',['../classBondRenderer.html#a857d2c2ae7a9e8ee8db5ab64011f3cf5',1,'BondRenderer']]],
+  ['build_4',['build',['../classHierarchicalTriangularMesh.html#a1a3cef433ecdd66eeeaa603eeb7aa617',1,'HierarchicalTriangularMesh']]]
 ];
