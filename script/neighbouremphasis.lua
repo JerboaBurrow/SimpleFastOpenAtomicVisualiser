814bac00a021f44c6fb33d9b2b4f820ad8db4a99
Jerboa-app <dev@jerboa.app>
2025-02-26 19:34:34 +0000

    Add neighbour emphasis script

---
+++ script/neighbouremphasis.lua
@@ -0,0 +1,14 @@
+-- Get atom 0's neighbours up to 16 Angstroms.
+neighbours = sfoav.getAtomsNeighbours(0, 4.0)
+
+-- Fade all atoms.
+for i = 1, sfoav.atomCount() do
+    r, g, b, a = sfoav.getAtomColour(i-1)
+    sfoav.setAtomColour(i-1, r, g, b, 0.1)
+end
+
+-- Emphasise all neighbours to 0 withint 16 Angstroms.
+for i = 1, #neighbours do
+    r, g, b, a = sfoav.getAtomColour(neighbours[i])
+    sfoav.setAtomColour(neighbours[i]["index"], r, g, b, 1.0);
+end
