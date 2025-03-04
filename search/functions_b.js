64204816b31365e8239d67b276435b1455b9059b
Jerboa-app <84378622+Jerboa-app@users.noreply.github.com>
2025-03-04 08:57:28 +0000

    Deploying to docs from @ JerboaBurrow/SimpleFastOpenAtomicVisualiser@6215407ee222fc0b86db9305a81ec05a63694552 🚀

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['neighbours_0',['neighbours',['../classNeighbours.html#a3d721788dbd8a373fe7b8f838ec96109',1,'Neighbours::Neighbours(const std::vector&lt; Atom &gt; &amp;atoms, float domainSideLength=-1.0)'],['../classNeighbours.html#a553a28193a111fe06053c77aa0990394',1,'Neighbours::neighbours(const std::vector&lt; Atom &gt; &amp;atoms, glm::vec3 position, float cutoff, bool noDirect=false) const']]],
-  ['neighboursdirect_1',['neighboursDirect',['../classNeighbours.html#ac30fb4eb2179c46be4c2e1b48914902a',1,'Neighbours']]],
+  ['neighbours_0',['neighbours',['../classNeighbours.html#a3d721788dbd8a373fe7b8f838ec96109',1,'Neighbours::Neighbours(const std::vector&lt; Atom &gt; &amp;atoms, float domainSideLength=-1.0)'],['../classNeighbours.html#a5a0c4099ef9df82dabfd104219abe2a2',1,'Neighbours::neighbours(const std::vector&lt; Atom &gt; &amp;atoms, glm::vec3 position, float cutoff, bool noDirect=false, bool nearestImage=true) const']]],
+  ['neighboursdirect_1',['neighboursDirect',['../classNeighbours.html#a061cd3740f9ce96333be645001cf4c98',1,'Neighbours']]],
   ['normal_2',['normal',['../classTriangle.html#a99cd8849b315038679c52528b4567730',1,'Triangle']]]
 ];
