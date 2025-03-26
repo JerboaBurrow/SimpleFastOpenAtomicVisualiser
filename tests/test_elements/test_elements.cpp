389517efd3fa57c8e22cfee253b83bcd5910ddce
Jerboa-app <dev@jerboa.app>
2025-03-26 19:23:39 +0000

    Support atom names
    Also account for case

--- tests/test_elements/test_elements.cpp
+++ tests/test_elements/test_elements.cpp
@@ -1,4 +1,5 @@
 #include <colour.h>
+#include <element.h>
 
 void checkVec4(glm::vec4 actual, glm::vec4 exected, double tol);
 std::string randomFileName();
@@ -33,6 +34,66 @@ SCENARIO("Colourmap reading")
                     checkVec4(ec.second, CPK_COLOURS.at(ec.first));
                 }
             }
+        }
+    }
+}
+
+SCENARIO("String to Element")
+{
+    GIVEN("The string C")
+    {
+        THEN("stringSymbolToElement(\"C\") is Element::C")
+        {
+            REQUIRE(stringSymbolToElement("C") == Element::C);
+        }
+    }
+    GIVEN("The string Car")
+    {
+        THEN("stringSymbolToElement(\"Car\") is Element::C")
+        {
+            REQUIRE(stringSymbolToElement("Car") == Element::C);
+        }
+    }
+    GIVEN("The string CAR")
+    {
+        THEN("stringSymbolToElement(\"CAR\") is Element::C")
+        {
+            REQUIRE(stringSymbolToElement("CAR") == Element::C);
+        }
+    }
+    GIVEN("The string Calc")
+    {
+        THEN("stringSymbolToElement(\"Calc\") is Element::Ca")
+        {
+            REQUIRE(stringSymbolToElement("Calc") == Element::Ca);
+        }
+    }
+    GIVEN("The string NLT")
+    {
+        THEN("stringSymbolToElement(\"NLT\") is Element::N")
+        {
+            REQUIRE(stringSymbolToElement("NLT") == Element::N);
+        }
+    }
+    GIVEN("The string OW")
+    {
+        THEN("stringSymbolToElement(\"OW\") is Element::O")
+        {
+            REQUIRE(stringSymbolToElement("OW") == Element::O);
+        }
+    }
+    GIVEN("The string OXYGEN")
+    {
+        THEN("stringSymbolToElement(\"OXYGEN\") is Element::O")
+        {
+            REQUIRE(stringSymbolToElement("OXYGEN") == Element::O);
+        }
+    }
+    GIVEN("The string OXYGENHYDROGEN")
+    {
+        THEN("stringSymbolToElement(\"OXYGENHYDROGEN\") is Element::O")
+        {
+            REQUIRE(stringSymbolToElement("OXYGENHYDROGEN") == Element::O);
         }
     }
 }
