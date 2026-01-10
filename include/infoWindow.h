d4d6f6402b60ba975d32c7408878e159dcef9568
Jerboa-app <dev@jerboa.app>
2026-01-10 07:55:48 +0000

    Fix bugs
    Video not recording frames.
    Focussing of console.
    BondRenderer and info deltas use before initialise errors.

--- include/infoWindow.h
+++ include/infoWindow.h
@@ -35,12 +35,15 @@ void infoWindow
     const std::unique_ptr<Structure> & structure,
     const Camera & camera,
     const VisualisationState & visualisationState,
-    const float & delta
+    const float delta
 )
 {
     auto cx = fixedLengthNumber(camera.position().x, 6);
     auto cy = fixedLengthNumber(camera.position().y, 6);
     auto cz = fixedLengthNumber(camera.position().z, 6);
+
+    auto ift = fixedLengthNumber(delta,6);
+    auto fps = fixedLengthNumber(1.0/(delta*1e-3),4);
 
     ImGui::Begin("Information");
 
@@ -49,7 +52,7 @@ void infoWindow
     ImGui::Text("Frame %*lu / %*lu", size, visualisationState.frame+1, size, structure->frameCount());
     ImGui::Text("Frame caching %s", (structure->framePositionsLoaded() ? "complete." : "in progress."));
     ImGui::Text("Camera %s, %s, %s", cx.c_str(), cy.c_str(), cz.c_str());
-    ImGui::Text("Frame Time (FPS) %s ms (%s)", fixedLengthNumber(delta,6).c_str(), fixedLengthNumber(1.0/(delta*1e-3),4).c_str());
+    ImGui::Text("Frame Time (FPS) %s ms (%s)", ift.c_str(), fps.c_str());
 
     ImGui::End();
 }
